5cf4a879085341492a8c0268e2bd2e724c703b01
Alexander Dunn <Alexander.Dunn@LibertyMutual.com>
2024-07-17 15:51:59 -0400

    add MainApplication in case we need it

---
+++ app/src/main/java/com/libertymutual/android/interview/MainApplication.kt
@@ -0,0 +1,9 @@
+package com.libertymutual.android.interview
+
+import android.app.Application
+
+class MainApplication: Application() {
+    override fun onCreate() {
+        super.onCreate()
+    }
+}
