bdb4ff35d2cd4df4e2cf2a7a1ca1c4976e003449
Alexander Dunn <Alexander.Dunn@LibertyMutual.com>
2024-07-17 15:20:55 -0400

    little updates, move to uiState

--- app/src/main/java/com/libertymutual/android/interview/ui/datelist/DateListViewModel.kt
+++ app/src/main/java/com/libertymutual/android/interview/ui/datelist/DateListViewModel.kt
@@ -3,9 +3,14 @@ package com.libertymutual.android.interview.ui.datelist
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import com.libertymutual.android.interview.data.DateItem
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.update
 
 class DateListViewModel : ViewModel() {
-    val dateItems = MutableLiveData<List<DateItem>>()
+
+    private val _uiState = MutableStateFlow(UIState(dateItems = emptyList()))
+    val uiState: StateFlow<UIState> =_uiState
 
     fun onViewCreated() {
         setupMockDataForList()
@@ -14,6 +19,10 @@ class DateListViewModel : ViewModel() {
     private fun setupMockDataForList() {
         val mockDates = listOf("1/1/2022", "1/2/2022", "1/3/2022", "1/4/2022")
         val mockDateItems = mockDates.map { date -> DateItem(date) }
-        dateItems.value = mockDateItems
+        _uiState.update { it.copy(dateItems = mockDateItems) }
     }
 }
+
+data class UIState (
+    val dateItems: List<DateItem>
+)
